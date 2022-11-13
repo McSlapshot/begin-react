ca39e6053023dbad3cce9a00fd3d56158b60a01d
Sia McQuillan <mcqui064@d.umn.edu>
2022-11-13 10:46:42 -0600

    starting react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Nav from './Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: null, 
+      post: [],
+      name: 'Sia',
+      age: 22
+      }
+    console.log('consrrustion is done')
+  }
+
+  componentDidMount = () => {
+    console.log('first rendering is completed (MOUNTED!)')
+  }
+  methodNumber3 = () => {
+    return 'do something'
+  }
+  happyBirthday = () => {
+    console.log('button is clicked')
+    this.setState({age: this.state.age + 1})
+  }
+  render() {
+    console.log('rendering is about to happen')
+
+    return (
+      <div>
+        <Nav/>
+          <h1>Wasssuppppp</h1>
+          <h3>My name is {this.state['name']}, I am {this.state['age']} years old</h3>
+          <button onClick={this.happyBirthday} >Add to age +</button>
+      </div>
+    )
+  }
 }
-
-export default App;
